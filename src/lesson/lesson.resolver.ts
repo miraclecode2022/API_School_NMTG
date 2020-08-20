d4434b955cfd0411bd310aaf8cd6dd6d49408ac0
Matthew <gialonghypercode@gmail.com>
2020-08-20 11:13:34 +0700

    Update full

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,13 +1,25 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './lesson.input';
 import { AssignStudentsToLessonInput } from './assign-student-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { StudentService } from 'src/student/student.service';
 
 // Resolver giống như là controller vậy
 @Resolver(of => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
   @Query(returns => LessonType)
   lesson(@Args('id') id: string) {
@@ -35,5 +47,11 @@ export class LessonResolver {
     return this.lessonService.assignStudentsToLesson(
       assignStudentsToLessonInput,
     );
+  }
+
+  // khi cần access path students trong lesson thì phải tạo như này
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
